14e4cf671a232b017864525cace4bcaf1c3d700e
Rodrigo Rodriguez <rrodriguez@growthaccelerationpartners.com>
2013-11-25 14:45:18 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsContinuosTraining::Application.config.secret_token = '60192b5e35c2ce63d38930fe00dc60fa40dde219a88ee5fd90360a51f2a1d3bd7089b555ff9cda7f2fc426e3156bc64298959ef164eb210fc10b1de5ecdd2d97'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+RailsContinuosTraining::Application.config.session_store :cookie_store, key: '_rails_continuos_training_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# RailsContinuosTraining::Application.config.session_store :active_record_store
